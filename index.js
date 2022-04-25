449ab6e2899b9998072c081b13614724e4127113
Alknight17 <knightalex17@gmail.com>
2022-04-25 19:06:15 -0400

    create user prompts

--- index.js
+++ index.js
@@ -1,7 +1,97 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+const generateReadme = require('./utils/generateMarkdown');
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: `Please write a description of your project`,
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: `What's your GitHub User Name?`,
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What's your email address?`,
+    },
+    
+    {
+        type: 'confirm',
+        name: 'install',
+        message: `Do you want to add any installation notes?`,
+
+    },
+    {
+        type: 'input',
+        name: 'installNotes',
+        message: `Please add your installation notes`,
+        when: function (answers) {
+            return answers.install;
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'usage',
+        message: `Do you want to provide the user usage information?`,
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: `Please add your usage info`,
+        when: function (answers) {
+            return answers.usage;
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'contrib',
+        message: `Do you want to add any notes on contributing to the repo?`,
+    },
+    {
+        type: 'input',
+        name: 'contribNotes',
+        message: `Please add your what you want the user to know about contributing to the repo`,
+        when: function (answers) {
+            return answers.contrib;
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'test',
+        message: `Do you want to add instructions for running tests?`,
+    },
+    {
+        type: 'input',
+        name: 'testNotes',
+        message: `Please add your instructions for running tests`,
+        when: function (answers) {
+            return answers.test;
+        }
+    },
+    {
+        type: 'rawlist',
+        name: 'license',
+        message: 'Which open source license would you like to use? ',
+        choices: ['Apache', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU AGPL', 'GNU GPL', 'GNU GPLv', 'MIT', 'Mozilla Public'],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
